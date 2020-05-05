a2693dd13dab55751921677817b44de8b055a694
yipenglai <lai.yipeng@hotmail.com>
2020-05-04 23:32:49 -0400

    Update README.md

--- README.md
+++ README.md
@@ -5,3 +5,14 @@ Most of the popular methods used for learning word representations only consider
 ## Data
 * Training data: [Chinese Wikipedia dump](https://dumps.wikimedia.org/zhwiki/latest/zhwiki-latest-pages-articles.xml.bz2)
 * Evalution data: Chinese Word similarity tasks [`wordsim-240` and `wordsim-296`](https://github.com/HKUST-KnowComp/JWE/tree/master/evaluation)
+
+## Quick Start
+### Install packages
+Run `pip install -r requirements`
+
+### Preprocess Wiki Dump
+After downloading the latest Chinese Wikipedia dump from the link above, run `python preprocess_wiki.py --input zhwiki-latest-pages-articles.xml.bz2 --output zhwiki_tokenized.txt` to
+* Convert traditional Chinese to simplified Chinese
+* Remove non-Chinese characters including punctuations and spaces
+* Split sentences into words separated by space
+* Convert xml into txt file
--- convert_subchar.py
+++ convert_subchar.py
@@ -28,25 +28,25 @@ def main():
 
     parser = argparse.ArgumentParser(description='Evaluate word representations')
     parser.add_argument('--input', type=str, default='zhwiki_tokenized.txt', help='Tokenized Wiki file path')
-    parser.add_argument('--output', type=str, default='zhwiki_subword.txt', help='Output file path')
-    parser.add_argument('--subword', type=str, default='radical', help='Component to be extracted {radical, stroke, wubi}')
+    parser.add_argument('--output', type=str, default='zhwiki_subchar.txt', help='Output file path')
+    parser.add_argument('--subchar', type=str, default='radical', help='Component to be extracted {radical, stroke, wubi}')
     args = parser.parse_args()
 
-    logging.info('Start extracting {} from {}'.format(args.subword, args.input))
+    logging.info('Start extracting {} from {}'.format(args.subchar, args.input))
     input = open(args.input, 'r')
     output = open(args.output, 'w')
     i = 0
     for article in tqdm(input):
-        if args.subword == 'radical':
-            subword = convert_radical(article)
-        elif args.subword == 'stroke':
-            subword = convert_stroke(article)
-        elif args.subword == 'wubi':
-            subword = convert_wubi(article)
+        if args.subchar == 'radical':
+            subchar = convert_radical(article)
+        elif args.subchar == 'stroke':
+            subchar = convert_stroke(article)
+        elif args.subchar == 'wubi':
+            subchar = convert_wubi(article)
         else:
-            logging.error('Please enter the correct subword component {radical, stroke, wubi}')
+            logging.error('Please enter the correct subcharacter component {radical, stroke, wubi}')
             break
-        output.write(subword + '\n')
+        output.write(subchar + '\n')
         i += 1
         if (i % 10000 == 0):
             logging.info('Finished processing {} articles'.format(i))
